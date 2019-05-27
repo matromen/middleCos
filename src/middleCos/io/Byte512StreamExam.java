2a6a992f66eeeaf28746311a00142103ad4b5c03
matromen <matromen@naver.com>
2019-05-28 05:41:14 +0900

    first commit

---
+++ src/middleCos/io/Byte512StreamExam.java
@@ -0,0 +1,50 @@
+package middleCos.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Byte512StreamExam {
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		
+		try {
+			fis = new FileInputStream(new File("src/middleCos/io/ByteStreamExam.java"));
+			fos = new FileOutputStream(new File("byte.txt"));
+			
+			
+			int read = -1;
+			byte[] bytes = new byte[512];
+			
+			while( (read = fis.read(bytes)) != -1 ) {
+				fos.write(bytes, 0, read);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fis.close(); 
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println(endTime-startTime);
+	}
+}
