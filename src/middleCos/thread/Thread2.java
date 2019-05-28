65686e19779afda3b5e669a26ef2d8e2340b2ac8
matromen <matromen@naver.com>
2019-05-29 06:28:38 +0900

    thread2 수정

--- src/middleCos/thread/Thread2.java
+++ src/middleCos/thread/Thread2.java
@@ -8,16 +8,14 @@ public class Thread2 implements Runnable{
 	}
 	
 	@Override
-	
-	() -> {
+	public void run(){
 		for(int i=0; i<100; i++) {
 			try {
 				Thread.sleep((int)(Math.random()*1000));
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println(this.name + ", " + i);
+			System.out.println("스레드 명 : " + this.name + ", " + i);
 		}		
-	};
+	}
 }
